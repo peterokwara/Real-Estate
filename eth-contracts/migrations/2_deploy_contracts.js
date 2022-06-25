83c4205efdc5cddeedd8f9d3fa546c782e13eb88
Peter Okwara <ojakaapeter@gmail.com>
2022-06-25 12:37:39 +0300

    Create script to mint 10 tokens for a single account

--- eth-contracts/migrations/2_deploy_contracts.js
+++ eth-contracts/migrations/2_deploy_contracts.js
@@ -1,7 +1,9 @@
 // migrating the appropriate contracts
-var Verifier = artifacts.require("./Verifier.sol");
-var ERC721Token = artifacts.require("./ERC721Token.sol");
-var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
+const Verifier = artifacts.require("./Verifier.sol");
+const ERC721Token = artifacts.require("./ERC721Token.sol");
+const SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
+const fs = require('fs');
+
 
 module.exports = async (deployer) => {
 
@@ -13,5 +15,13 @@ module.exports = async (deployer) => {
 
   await deployer.deploy(SolnSquareVerifier, verifierContract.address);
   const solnSquareVerifier = await SolnSquareVerifier.deployed();
+
+
+  let config = {
+    url: 'http://localhost:8545',
+    solnSquareVerifierAddress: solnSquareVerifier.address,
+  }
+
+  fs.writeFileSync(__dirname + '/../data/config.local.json', JSON.stringify(config, null, '\t'), 'utf-8');
 }
 
---
+++ eth-contracts/mint.js
@@ -0,0 +1,109 @@
+
+
+const { ethers } = require("ethers");
+const config = require("./data/config.local.json");
+let solnSquareVerifier = require("./build/contracts/SolnSquareVerifier.json");
+
+// Url where the contract is deployed
+// const url = "https://rinkeby.infura.io/v3/a6cb341cf6d94672a24485f50d2fa95a" // rinkeby
+// const appAddress = "0x72266125eA9F5a4a46F9f908F964a31760d703a7"; // rinkeby
+
+const url = config.url; // localhost
+const appAddress = config.solnSquareVerifierAddress;
+
+
+
+let web3Provider;
+let accounts;
+let owner;
+let realEstateContract;
+
+
+
+/**
+ * Initialize web3
+ */
+async function initWeb3() {
+
+    // Load web3 provider
+    web3Provider = new ethers.providers.JsonRpcProvider(url);
+
+    // Get all accounts
+    accounts = await web3Provider.listAccounts();
+
+    // Set the owner account
+    owner = await accounts[0];
+
+    // Get the signer
+    const signer = await web3Provider.getSigner();
+
+    // Get an instance of the contract
+    realEstateContract = new ethers
+        .Contract(appAddress, solnSquareVerifier.abi, signer);
+
+}
+
+/**
+ * Add a new solution
+ */
+async function submitSolution(tokenID, zokratesProof) {
+
+    // Set the signer
+    const signer = web3Provider.getSigner(owner);
+
+    // Set the contract
+    const contract = realEstateContract.connect(signer);
+
+    try {
+        const transaction = await contract
+            .submitSolution(...Object.values(zokratesProof.proof), zokratesProof.inputs, accounts[1], tokenID);
+        await transaction.wait();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+/**
+ * Mint a new token
+ */
+async function mint(tokenID) {
+
+    // Set the signer
+    const signer = web3Provider.getSigner(owner);
+
+    // Set the contract
+    const contract = realEstateContract.connect(signer);
+
+    try {
+        const transaction = await contract.mint(accounts[1], tokenID);
+
+        await transaction.wait();
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+
+async function run() {
+
+    // Initialize web3
+    await initWeb3();
+
+    // Mint 10 tokens for a single account
+    for (let index = 1; index < 11; index++) {
+
+        // Set the proof to use
+        let zokratesProof = require(`../zokrates/code/square/proof_${index}.json`);
+
+        // Submit a solution
+        await submitSolution(index, zokratesProof);
+
+        // Mint a token
+        await mint(index);
+
+    }
+
+}
+
+run();
